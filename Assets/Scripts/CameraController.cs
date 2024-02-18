b02e1612220ed95fca8e9593ef107994660f0172
parsley <nhanson@posteo.net>
2024-02-18 01:57:28 -0500

    Implement mouselook

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,17 +6,36 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField]
     float cameraMoveSpeed = 50f;
+    [SerializeField]
+    float mouseSensitivity = 50f;
+
+    float pitch;
+    float yaw;
 
     // Start is called before the first frame update
     void Start()
     {
+        pitch = transform.rotation.eulerAngles.x;
+        yaw = transform.localRotation.eulerAngles.y;
 
+        Debug.Log(pitch);
+        Debug.Log(yaw);
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Moving the camera on the X/Z axis
+        // Input handling for camera move and rotation
+        CameraMove();
+        CameraRotate();
+    }
+
+    // Handles movement of the camera on all axises.
+    void CameraMove()
+    {
         float xTranslate = Input.GetAxis("Horizontal") * cameraMoveSpeed;
         float zTranslate = Input.GetAxis("Vertical") * cameraMoveSpeed;
         float yTranslate = 0f;
@@ -30,7 +49,26 @@ public class CameraController : MonoBehaviour
             yTranslate = -cameraMoveSpeed;
         }
 
+        Vector3 translation = new Vector3(xTranslate * Time.deltaTime, yTranslate * Time.deltaTime, zTranslate * Time.deltaTime);
 
-        transform.Translate(xTranslate * Time.deltaTime, yTranslate * Time.deltaTime, zTranslate * Time.deltaTime);
+        transform.Translate(translation);
+    }
+
+    // Handles rotation of the camera utilizing the mouse
+    void CameraRotate()
+    {
+        // Only allow camera rotation when right clicking
+        if (Input.GetMouseButton(1))
+        {
+            float xRotate = Input.GetAxis("Mouse X") * mouseSensitivity;
+            float yRotate = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+            yaw += xRotate;
+
+            pitch -= yRotate;
+            pitch = Mathf.Clamp(pitch, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(pitch, yaw, 0f);
+        }
     }
 }
